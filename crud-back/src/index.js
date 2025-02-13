f2558398963180b4c192032d531f992c4990b807
Yuvraj Singh <77329747+zyuvraj53@users.noreply.github.com>
2025-02-14 05:27:22 +0530

    Update index.js

--- crud-back/src/index.js
+++ crud-back/src/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import clientRoutes from './routes/clientRoute.js'
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
