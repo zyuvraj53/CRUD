5ea2f6635370535640bac54fd202a4e724b910bf
zyuvraj53 <zyuvraj53@gmail.com>
2025-02-14 05:15:31 +0530

    case change

--- crud-front/src/App.jsx
+++ crud-front/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import ModalForm from "./components/Modalform";
+import ModalForm from "./components/ModalForm";
 import NavBar from "./components/Navbar";
-import TableList from "./components/Tablelist";
+import TableList from "./components/TableList";
 import axios from "axios";
 
 function App() {
